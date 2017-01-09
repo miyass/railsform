be3bd10f254ec725af20cf1506f914861f72647c
Sohei <exe.miyakura0629@gmail.com>
2017-01-09 05:06:04 +0000

    デバイス更新

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,7 +1,35 @@
 class TweetsController < ApplicationController
     
+    before_action :move_to_index, except: :index
+    
     def index
-        @tweet = Tweet.all
+        @tweet = Tweet.all.order("id ASC")
+                                #("テーブルのカラム名",ASC or DESC  )
+        # ASC...昇順
+        #DESC...降順
     end
+    
+    def new
+        
+    end
+    
+    def create
+        Tweet.create(tweet_params)
+    end
+    
+    private
+    def tweet_params
+        params.permit(:name, :iamge, :text)
+    end
+    
+    def move_to_index
+        # ログインしていない時、「index」アクションを実行
+        unless user_signed_in?  
+            redirect_to action: :index
+        end
+    end
+    
+    
+    
     
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  get 'tweets' => 'tweets#index'  
+  devise_for :users
+  root 'tweets#index'
+  get 'tweets/new' => 'tweets#new'
+  post 'tweets' => 'tweets#create'
 end
